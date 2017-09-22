f96eddd3e994a0bcba6c6ddb0f9cb5121e37f362
SinceJuly <31611146+SinceAlone@users.noreply.github.com>
2017-09-22 11:00:25 +0800

    Add files via upload

--- 115040100421.cpp
+++ 115040100421.cpp
@@ -1,19 +1,17 @@
 #include<iostream.h>
 void main()
 {
-	int i=1,x;
+	int x,y;
 	cout<<"please input x:"<<endl;
-	cin>>x;
-	while(x>10)
-	{
-	x=x/10;
-	i=i++;
-	}
-	switch(i)
-	{
-		case 1:cout<<"x<10"<<endl;break;
-		case 2:cout<<"10<=x<=99"<<endl;break;
-		case 3:cout<<"100<=x<=999"<<endl;break;
-		default:cout<<"x>1000"<<endl;
-	}
+		cin>>x;
+	if(x<10)
+		cout<<"y=1"<<endl;
+	else
+		if(x<100)
+			cout<<"y=2"<<endl;
+		else
+			if(x<1000)
+				cout<<"y=3"<<endl;
+			else
+				cout<<"y=4"<<endl;
 }
