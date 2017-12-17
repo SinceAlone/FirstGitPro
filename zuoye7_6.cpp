21df821928f220871b61000f2ce69682eff42e0a
SinceJuly <31611146+SinceAlone@users.noreply.github.com>
2017-12-17 22:26:41 +0800

    Add files via upload

---
+++ zuoye7_6.cpp
@@ -0,0 +1,32 @@
+#include<iostream.h>
+#include<stdio.h>
+#include<string.h>
+void main()
+{
+	int i,j=0,k,x;
+	char *a[30],*b[30];
+	char c[60];
+	cout<<""<<endl;
+	gets(*a);
+	cout<<""<<endl;
+	gets(*b);
+	cout<<""<<endl;
+	cin>>k;
+	for(i=k;i<float(b);i++)
+	{
+		c[j++]=*a[i];
+	}
+	x=j;
+	j=n;
+	for(i=0;i<b;i++)
+	{
+		a[j++]=b[i];
+	}
+	for(i=0;i<x;i++)
+	{
+		a[j++]=c[i];
+	}
+	a[j]='\0';
+	puts(a);
+}
+
